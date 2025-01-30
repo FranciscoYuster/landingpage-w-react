2accbbc314caede2a2ea6381ca86ca7327942d59
FranciscoYuster <yusterfrancisco@gmail.com>
2025-01-30 18:07:13 -0300

    upload

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -4,7 +4,7 @@ const Card = (props) => {
     const { image, title, description } = props.cards;
     return (
         <div className="card h-100">
-            <img src={image} className="card-img-top" style={{ height: '200px', objectFit: 'cover' }} alt={title} />
+            <img src={"https://picsum.photos/id/"+(Math.floor(Math.random()*900)+100)+"/400/400"} className="card-img-top" style={{ height: '200px', objectFit: 'cover' }} alt={title} />
             <div className="card-body"> 
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{description}</p>
